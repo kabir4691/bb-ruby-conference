fd09eaeb0b9c95940c0f0ac0286a8441e5e977a1
Kabir <kabir4691@gmail.com>
2020-08-03 10:26:47 +0530

    Implement feature to add talk to session if session is not full

---
+++ session.rb
@@ -0,0 +1,25 @@
+require_relative 'talk'
+
+class Session
+
+  def initialize(name, start_time, end_time)
+    @name = name
+    @start_time = start_time
+    @end_time = end_time
+    @talks = []
+    @available_minutes = (end_time.to_i - start_time.to_i) / 60
+  end
+
+  def add_talk(talk)
+    return false if can_add_talk(talk) == false
+
+    @talks << talk
+    @available_minutes -= talk.minutes
+
+    return true
+  end
+
+  def can_add_talk(talk)
+    return talk.minutes <= @available_minutes
+  end
+end
---
+++ talk.rb
@@ -0,0 +1,9 @@
+class Talk
+
+  attr_reader :name, :minutes
+
+  def initialize(name, minutes)
+    @name = name
+    @minutes = minutes
+  end
+end
